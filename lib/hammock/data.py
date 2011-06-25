b5da195008e97b3627f3f764d7929e7eaad3f61e
mvr <matthewvonrocketstein@gmail-dot-com>
2011-06-25 17:34:15 -0400

    [master] breaking up __init__ file

--- lib/hammock/data.py
+++ lib/hammock/data.py
@@ -3,5 +3,5 @@ DEBUG       = True
 SERVER      = 'http://dojo.robotninja.org:5984/'
 CREDENTIALS = ('matt', 'lemmein')
 CONTROL_T   = "<br/><span class=control_class>{link}</span>"
-
-center_zoom=6
+SECRET_KEY  = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+center_zoom = 6
---
+++ lib/hammock/plumbing.py
@@ -0,0 +1,16 @@
+from flask import render_template, abort, g, flash
+from flask import Flask, request, session, url_for, redirect
+
+def before_request():
+    """ Make sure we are connected to the database
+        each request and look up the current user
+        so that we know he's there.
+    """
+    g.user = None
+    if 'user_id' in session:
+        print 'logged in'
+        g.user='superuser'
+
+def after_request(response):
+    """ nothing to do here so far. """
+    return response
