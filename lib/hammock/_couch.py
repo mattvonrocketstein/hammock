bd7665c300e585c4db4f924ce44bdc01fe025cae
matt <matt@cmg>
2011-06-25 22:48:21 -0400

    [master] added _couch file for couchdb specific stuff

---
+++ lib/hammock/_couch.py
@@ -0,0 +1,31 @@
+""" /home/matt/code/hammock/lib/hammock/_couch.py
+"""
+from hammock.data import *
+from hammock.util import report
+
+def update_db(db, _id, dct):
+    """  stupid.. have to delete and restore instead of update? """
+    before = db[_id]
+    before = dict(before.items())
+    before.pop('_rev')
+    before.update(dct)
+    del db[_id]
+    db[_id] = before
+    report("updated {id} with new values for keys".format(id=_id), dct.keys())
+
+def setup():
+    """ couch-specific stuff """
+    import couchdb
+    global couch
+    couch = couchdb.Server(SERVER)
+    couch.resource.credentials = CREDENTIALS
+    return couch
+
+def coordinates(db):
+    return filter(lambda x: not x.startswith('_design'), db)
+
+def handle_dirty_entry(_id):
+    """ page at / may call this handler on malformed database entries. """
+    report('dirty entry in coordinates database.. removing it',[_id])
+    db = couch['coordinates']
+    del db[_id]
