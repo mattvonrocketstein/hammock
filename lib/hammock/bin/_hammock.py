1eaf7b7b85ea6e48cf2c5f2f6fa35d5d82a0e7e2
matt <matt@cmg>
2011-06-28 01:45:02 -0400

    [master] doh removing ipython term

--- lib/hammock/bin/_hammock.py
+++ lib/hammock/bin/_hammock.py
@@ -21,7 +21,7 @@ def PARSER():
 def entry():
     """ Main entry point """
     from hammock import app
-    from IPython import Shell; Shell.IPShellEmbed(argv=['-noconfirm_exit'])()
+    #from IPython import Shell; Shell.IPShellEmbed(argv=['-noconfirm_exit'])()
     #app.run()#def run(self, host='127.0.0.1', port=5000, **options):
     app.run(host='0',port=5000)
 
