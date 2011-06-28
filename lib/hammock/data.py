6f3a92d40379e3a1f812d82e559a7431c4ea7bf5
matt <matt@cmg>
2011-06-28 01:54:58 -0400

    [master] gahh forgot to save this file

--- lib/hammock/data.py
+++ lib/hammock/data.py
@@ -5,4 +5,5 @@ SERVER      = 'http://dojo.robotninja.org:5984/'
 CREDENTIALS = ('matt', 'lemmein')
 CONTROL_T   = "<br/><span class=control_class>{link}</span>"
 SECRET_KEY  = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+MAPS_API_KEY = 'ABQIAAAAGGWUIcktQOU5Q76O6cV_BhRFeVNa246MatoBH1bx3V0-W8LG9hTZAWkoAFU1qTLCQuQOI0mPSlWsXQ'
 center_zoom = 6
