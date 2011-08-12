c691981f8417498c5082187cde99c7692af37eb5
mvr <matthewvonrocketstein@gmail-dot-com>
2011-08-12 10:54:28 -0400

    [settings] refactored a bit, retested all the ajax stuff

--- lib/hammock/bin/_hammock.py
+++ lib/hammock/bin/_hammock.py
@@ -37,18 +37,9 @@ def go():
 
     ## Begin flask setup
     app = Flask('hammock')
-    app.config.from_object('hammock')#__name__)
+    app.config.from_object('hammock')
     app.secret_key = settings['flask.secret_key']
 
-    # HACK.. looking at template_folder arg passed to app()
-    #import os
-    #from jinja2 import FileSystemLoader
-    #ROOT = os.path.join(os.path.split(__file__)[0],'..')
-    #template_path = os.path.join(ROOT,'templates')
-    #app.jinja_loader = FileSystemLoader(template_path)
-    #app.static_folder =os.path.join(ROOT,'static')
-
-    ## Begin flask plumbing
     views.before_request = app.before_request(before_request)
     views.after_request  = app.after_request(after_request)
 
@@ -59,47 +50,9 @@ def go():
     views.slash          = app.route('/')(slash)
     views.remove = requires_authentication(app.route('/remove',methods=['POST'])(views.remove))
     views.set_location = requires_authentication(app.route('/set', methods=['GET', 'POST'])(views.set_location))
-    views.set_label = set_factory('label', app)
-    views.set_tag   = set_factory('tag', app)
+    views.set_label = views.set_factory('label', app)
+    views.set_tag   = views.set_factory('tag', app)
     return app
-
-    #@requires_authentication
-    #@app.route('/remove',methods=['POST'])
-    #@requires_authentication
-    #@app.route('/set', methods=['GET', 'POST'])
-
-def set_factory(attr,app):
-        """ """
-        def setter():
-            """ ajax -- sets an setter.attribute for a location
-                flask does something weird so that this closure doesn't
-                work the way it ought to.  hence we have to calculate 'attr'
-                based on request path :(
-            """
-            try:
-                this_attr = urlparse.urlsplit(request.url).path.split('_')[-1]
-
-                db    = get_db()
-                _id   = request.args.get('id')
-                report("in inner set for attribute {A} on id {I} with value {V}".format(I=_id,
-                                                                                        A=this_attr,
-                                                                                        V=request.form.keys()))
-
-                label = request.args.get(this_attr)
-                # 'or' needed because we don't want to set
-                # it as None if the ajax screws up
-                label = label or str(label)
-                report("in inner set with", [label])
-                update_db(db, _id, {this_attr:label})
-                return jsonify(result='ok')
-            except Exception, e:
-                traceback.print_exc(e)
-        url    = '/set_' + attr
-        setter = requires_authentication(setter)
-        setter = app.route(url)(setter)
-        setter.attr = attr
-        print " * built setter", [attr,url]
-        return requires_authentication(setter)
 
 def entry():
     """ Main entry point """
--- lib/hammock/views.py
+++ lib/hammock/views.py
@@ -1,10 +1,25 @@
-from flask import Flask, request, jsonify
+import logging
+log = logging.getLogger(__file__)
+
+import datetime
+import urlparse
+import traceback
+
+from flask import request, jsonify
+
+from hammock.auth import requires_authentication
+from hammock.util import report
+from hammock._couch import get_db, update_db
+
+
+
 def remove():
     """ remove a loction ajax """
     if request.method=='POST':
         _id = request.form['id']
         del get_db()[_id]
         return jsonify(result='ok')
+
 def set_location():
     """ sets a location ajax
 
@@ -14,6 +29,39 @@ def set_location():
         db = get_db()
         date_str = str(datetime.datetime.now())
         coords=request.form['coords'].replace('(','').replace(')','')
-        data = dict(coords=coords, timestamp=date_str, tag='default')
+        data = dict(coords=coords, timestamp=date_str, tag='recent')
         db[date_str] = data
         return jsonify(result='ok')
+
+def set_factory(attr,app):
+    """ """
+    def setter():
+        """ ajax -- sets an setter.attribute for a location
+            flask does something weird so that this closure doesn't
+            work the way it ought to.  hence we have to calculate 'attr'
+            based on request path :(
+        """
+        try:
+            this_attr = urlparse.urlsplit(request.url).path.split('_')[-1]
+
+            db    = get_db()
+            _id   = request.args.get('id')
+            report("in inner set for attribute {A} on id {I} with value {V}".format(I=_id,
+                                                                                    A=this_attr,
+                                                                                    V=request.form.keys()))
+
+            label = request.args.get(this_attr)
+            # 'or' needed because we don't want to set
+            # it as None if the ajax screws up
+            label = label or str(label)
+            report("in inner set with", [label])
+            update_db(db, _id, {this_attr:label})
+            return jsonify(result='ok')
+        except Exception, e:
+            traceback.print_exc(e)
+    url    = '/set_' + attr
+    setter = requires_authentication(setter)
+    setter = app.route(url)(setter)
+    setter.attr = attr
+    print " * built setter", [attr,url]
+    return requires_authentication(setter)
