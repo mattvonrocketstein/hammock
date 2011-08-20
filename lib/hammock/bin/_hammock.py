ee7c1a0fcbd30fc0d0c31d66b7c59f3fb5b74438
mvr <matthewvonrocketstein@gmail-dot-com>
2011-08-20 16:40:22 -0400

    refactoring, preparing to abstract hammock._flask

--- lib/hammock/bin/_hammock.py
+++ lib/hammock/bin/_hammock.py
@@ -3,34 +3,36 @@
 
     command line script
 """
-import logging
-log = logging.getLogger(__file__)
 
-import datetime
-import urlparse
-import traceback
-from flask import Flask, request, jsonify
+from flask import Flask
+from jinja2 import FileSystemLoader
 
 from hammock.util import report
 from hammock.auth import Login, Logout
 from hammock.plumbing import before_request, after_request
-
+from hammock.map_home import Slash
+from hammock._couch import get_db, update_db, setup
+from hammock import views
+from hammock.reflect import namedAny
 def go():
     from hammock.conf import settings
-    from hammock._couch import get_db, update_db, setup
-    from hammock import views
-    from hammock.map_home import Slash
-
-    ## Begin database setup
-    couch     = setup()
-
-    ## Begin flask setup
-    app = Flask('hammock')
-    app.config.from_object('hammock')
+    app_name = settings['flask.app']
+    app = Flask(app_name)
     app.secret_key = settings['flask.secret_key']
 
-    views.before_request = app.before_request(before_request)
-    views.after_request  = app.after_request(after_request)
+    if 'flask.template_path' in settings:
+        app.jinja_loader = FileSystemLoader(settings['template_path'])
+
+    if 'flask.before_request' in settings:
+        before_request = settings['flask.before_request']
+        before_request = namedAny(before_request)
+        app.before_request(before_request)
+
+    if 'flask.after_request' in settings:
+        after_request = settings['flask.after_request']
+        after_request = namedAny(after_request)
+        app.after_request(after_request)
+
     [ v(app=app) for v in
       [Slash,
        Login,
@@ -53,12 +55,12 @@ def entry():
     settings = conf.Settings()
     conf.settings = settings
 
-    if settings.shell:
+    if settings['user.shell']:
         from IPython import Shell; Shell.IPShellEmbed(argv=['-noconfirm_exit'])()
     else:
         app = go()
-        app.run(host=settings['hammock.host'],
-                port=int(settings['hammock.port']),
+        app.run(host=settings['flask.host'],
+                port=int(settings['flask.port']),
                 debug=settings['flask.debug'])
 
 if __name__=='__main__':
