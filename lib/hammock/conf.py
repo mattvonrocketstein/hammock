9a9fd4b0dbb4f726f4ca15f8f3fd59f7c0cd498b
mvr <matthewvonrocketstein@gmail-dot-com>
2011-08-31 05:18:54 -0400

    support for template literals embedded in views

--- lib/hammock/conf.py
+++ lib/hammock/conf.py
@@ -7,6 +7,7 @@ from corkscrew import Settings as _Settings
 
 class Settings(_Settings):
     DEFAULT_SETTINGS = os.path.abspath(os.path.join(os.path.split(__file__)[0], "hammock.ini"))
+    default_file = 'hammock.ini'
     def __init__(self,*args, **kargs):
         #ugh hack
         super(Settings,self).__init__(*args, **kargs)
