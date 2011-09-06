fecdb96bf3f4b8ebf51e94a32fc86ad099f17a9b
mvr <matthewvonrocketstein@gmail-dot-com>
2011-09-06 13:39:22 -0400

    basic tags for books

--- lib/hammock/views/books.py
+++ lib/hammock/views/books.py
@@ -1,5 +1,6 @@
 """ hammock.views.books
 
+    http://www.jamendo.com/en/album/27535
     http://blog.skitsanos.com/2009/11/jquerycouchjs-cheatsheet.html
     http://stackoverflow.com/questions/5982638/using-cherrypy-cherryd-to-launch-multiple-flask-instances
 """
@@ -14,26 +15,40 @@ class BookList(DBView):
     url = '/books'
     database_name = 'books'
 
+    def _all_unique_tags(self):
+        q = '''function(doc){emit(null, doc.%s);}'''%'tags'
+        out = reduce(lambda x,y: x+y,[x.value for x in self._db.query(q)])
+        out=set(out)
+        return out
+
     @use_local_template
     def main(self):
         """
+        tags: {%for t in tags%}
+        <a href="/books?tag={{t}}">{{t}}</a> |
+        {%endfor%}
+        <hr/>
         <table>
         {%for book in booklist%}
         <tr><td>{{book.index}}. {{book.author}}: {{book.title}} <small>{{book.tags}}</small></td></tr>
         {%endfor%}
         </table>
         """
-        return dict(booklist=[document2namedt(obj) for k,obj in self.rows])
-tagger = lambda tag: """
-function(doc){
-function inside(needle,haystack) {
-        for(var i = 0; i < this.length; i++) {
-            if(this[i] === needle) {
-                return i;
-            }
-        }
-        return -1;
-    };
+        return dict(tags=self._all_unique_tags(),
+                    booklist=[document2namedt(obj) for k,obj in self.rows])
 
-if(doc.tag=='"""+tag+"""' or inside('"""+tag+"""',doc.tags)){emit(null, doc);}
-}"""
+hrm = """
+        <script src="/static/jquery.couch.js"></script>
+        <script>
+        // run this before anything else with $.couch
+        $.couch.urlPrefix = "http://dojo.robotninja.org:5984/";
+        $.couch.login({
+        url: '/_session',
+        name: 'matt',
+        password: 'lemmein',
+        success: function() {
+        alert('Ready!');
+        }
+        });
+        </script>
+"""
