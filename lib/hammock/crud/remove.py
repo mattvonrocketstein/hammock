9f798dcdd01660ee9c107b35615dae3e76c6c64a
matt <matt@cmg>
2011-09-24 18:54:59 -0400

    update for crud

--- lib/hammock/crud/remove.py
+++ lib/hammock/crud/remove.py
@@ -5,6 +5,7 @@ from corkscrew import SmartView
 
 from hammock._couch import update_db
 from hammock.views.db import DBView
+from hammock.utils import authorization_required
 
 class Remove(SmartView, DBView):
     """
@@ -26,3 +27,13 @@ class Remove(SmartView, DBView):
     def main(self):
         del self._db[self['id']]
         return dict(result='ok')
+
+class Removable(object):
+    """ mixin for item deletion """
+
+    @authorization_required
+    def remove(self):
+        _id   = self['remove']
+        report("Removing {id} from {db}", id=_id, db=self.database_name)
+        del self._db[_id]
+        return jsonify(dict(ok='true'))
