00d5f3127a020f23211b249e05aa52ea57d91091
mvr <matthewvonrocketstein@gmail-dot-com>
2012-01-17 10:40:59 -0500

    minor changes

--- lib/hammock/conf.py
+++ lib/hammock/conf.py
@@ -9,6 +9,8 @@ from corkscrew import Settings as _Settings
 couch = lazyModule('hammock._couch')
 
 class Settings(_Settings):
+    """
+    """
     default_file   = 'hammock.ini'
     backup_options = dict(dest='backup_database',
                           default='',
@@ -17,13 +19,17 @@ class Settings(_Settings):
     extra_options  = ( ('--backup',), backup_options )
 
     def __init__(self,*args, **kargs):
-        # ugh hack
+        """
+            ugh hack
+        """
         super(Settings,self).__init__(*args, **kargs)
         from corkscrew.settings import settings
         from hammock import conf
         conf.settings = self
 
     def shell_namespace(self):
+        """
+        """
         dct = super(Settings,self).shell_namespace()
         dct.update(db=couch.Server())
         return dct
