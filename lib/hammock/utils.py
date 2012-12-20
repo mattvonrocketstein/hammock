41b664e3fc18e1f5a20780f14927c6cbd90ed034
mvr <matthewvonrocketstein@gmail-dot-com>
2012-12-20 13:19:32 -0500

    docs

--- lib/hammock/utils.py
+++ lib/hammock/utils.py
@@ -4,6 +4,13 @@
 from flask import render_template_string, redirect
 
 def use_local_template(func):
+    """ example usage:
+
+          @use_local_template
+          def main(self):
+              ''' this docstring will be used as a template '''
+              return dict(this_context_will_be_used='to render the template')
+    """
     def fxn(*args, **kargs):
         context = func(*args, **kargs)
         template = '{%extends "layout.html" %}{%block body%}<center>' + \
