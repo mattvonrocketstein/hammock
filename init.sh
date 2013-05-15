b7dbf56761de83e604f57c7b2b2391faa7831296
mvr <matthewvonrocketstein@gmail-dot-com>
2013-05-15 13:18:27 -0400

    sq

--- init.sh
+++
@@ -1,9 +0,0 @@
-# Assuming you have a fresh couchdb, this will add
-# the hammock user and initialize hammock's database
-USER="hammock_coordinates"
-PASSWD="whereintheworldiscarmensandiego"
-HOST="http://127.0.0.1:5984"
-curl -X PUT $HOST/database
-curl -X PUT $HOST/_config/admins/$USER -d '"$PASSWD"'
-HOST="http://$USER:$PASSWD@127.0.0.1:5984"
-curl -X PUT $HOST/hammock_coordinates
