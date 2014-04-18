4e3062b07f255b49c39c72af5c9e5b86692d0756
mvr <matthewvonrocketstein@gmail-dot-com>
2014-04-18 17:10:49 -0400

    move plumbing to corkscrew

--- lib/hammock/plumbing.py
+++ lib/hammock/plumbing.py
@@ -1,29 +1,2 @@
 """ hammock.plumbing
 """
-from flask import g
-from flask import session, request
-from hammock.utils import report2 as report
-
-registry = []
-
-def before_request():
-    """ Make sure we are connected to the database
-        each request and look up the current user
-        so that we know he's there.
-    """
-    g.user = None
-    print request.url
-    if request.values:
-        #report(str(request.values))
-        print request.values
-    if 'user_id' in session:
-        g.user = session['user_id']
-
-def after_request(response):
-    response.request = request
-    for f in registry:
-        response = f(response)
-    return response
-
-def register(f):
-    registry.append(f)
