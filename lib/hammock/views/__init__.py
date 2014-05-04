04d676e92c065d4cc4cee9f3f98ba1666008eded
mvr <matthewvonrocketstein@gmail-dot-com>
2014-05-04 16:20:59 -0400

    refactor

--- lib/hammock/views/__init__.py
+++ lib/hammock/views/__init__.py
@@ -1,32 +1,8 @@
 """ hammock.views
-
-    don't use settings in here! they aren't ready yet
 """
 
-from corkscrew.auth import Login, Logout
-from corkscrew.views import Favicon
-
-Login._template = """
-{% extends "layout.html" %}
-{% block title %}Sign In{% endblock %}
-{% block body %}
-  <h2>Sign In</h2>
-  {% if error %}<div class=error><strong>Error:</strong> {{ error }}</div>{% endif %}
-  <form action="" method=post>
-    <dl>
-      <dt>Username:
-      <dd><input type=text name=username size=30 value="{{ request.form.username }}">
-      <dt>Password:
-      <dd><input type=password name=password size=30>
-      <input type=hidden name=next value="{{request.values.next}}">
-    </dl>
-    <div class=actions><input type=submit value="Sign In"></div>
-  </form>
-{% endblock %}
-"""
-
-__views__= [
-    # corkscrew standard views
-    Favicon, Login, Logout,
-
-    ]
+#from corkscrew.auth import Login, Logout
+#from corkscrew.views import Favicon
+#__views__ = [
+#    # corkscrew standard views
+#    Favicon, Login, Logout, ]
